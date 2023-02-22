1e368e1a1b964b1307c6b6371d6c2d7bedbe0f14
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-23 00:03:51 +0300

    added check if link is active

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,20 @@ import {FC} from 'react'
 import Link from 'next/link'
 import {aboutRoute, documentationRoute, downloadRoute, sourceCodeRoute} from '@/utils/constants'
 import styles from './header.module.sass'
+import {useRouter} from 'next/router'
 
 const Header: FC = () => {
+	const router = useRouter()
+
 	return (
 		<header className={styles.header}>
 			<Link className={styles.link} href="/">
 				<img className={styles.img} draggable={false} src="/favicon.svg" alt=""/>
 			</Link>
-			<Link className={styles.link} href={downloadRoute}>Установить</Link>
-			<Link className={styles.link} href={sourceCodeRoute}>Исходный код</Link>
-			<Link className={styles.link} href={documentationRoute}>Документация</Link>
-			<Link className={styles.link} href={aboutRoute}>О проекте</Link>
+			<Link data-active={router.pathname == downloadRoute} className={styles.link} href={downloadRoute}>Скачать</Link>
+			<Link target="_blank" className={styles.link} href={sourceCodeRoute}>Исходный код</Link>
+			<Link data-active={router.pathname == documentationRoute} className={styles.link} href={documentationRoute}>Документация</Link>
+			<Link data-active={router.pathname == aboutRoute} className={styles.link} href={aboutRoute}>О проекте</Link>
 		</header>
 	)
 }
