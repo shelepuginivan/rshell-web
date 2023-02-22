79ecdd92fb61115ec92aca1876bc2aef493d4f11
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-22 23:06:12 +0300

    created section `SectionCommandInterpreter` component for main page

---
+++ src/components/HelloWorld/SectionCommandInterpreter.tsx
@@ -0,0 +1,25 @@
+import {FC} from 'react'
+import styles from './sectionCommandInterpreter.module.sass'
+
+const SectionCommandInterpreter: FC = () => {
+	return (
+		<section className={styles.helloWorld}>
+			<code>
+				<span>$ echo Hello, World! | @format --red --bold --italic</span>
+				<br/>
+				<span className={styles.formatted}>Hello, World!</span>
+			</code>
+
+			<h1>Командный интерпретатор</h1>
+			<blockquote>
+				<p>
+					rshell позволяет выполнять любые UNIX команды и запускать исполняемые файлы.
+					Используйте функционал оболочки для быстрого и эффективного взаимодействия
+					с операционной системой.
+				</p>
+			</blockquote>
+		</section>
+	)
+}
+
+export default SectionCommandInterpreter
