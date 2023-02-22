a8c1da58c2362bd1a4bf104294dd3efe7c9a055e
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-22 21:08:20 +0300

    added menu with links

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,10 +1,16 @@
+import RoundedLink from '@/ui/RoundedLink/RoundedLink'
 import {FC} from 'react'
 import styles from './intro.module.sass'
+import {documentationRoute, downloadRoute} from '@/utils/constants'
 
 const Intro: FC = () => {
 	return (
 		<section className={styles.intro}>
 			<h1><span>rshell</span> - Российская оболочка для UNIX систем</h1>
+			<menu>
+				<RoundedLink color="red" href={downloadRoute}>Скачать</RoundedLink>
+				<RoundedLink color="green" href={documentationRoute}>Документация</RoundedLink>
+			</menu>
 		</section>
 	)
 }
