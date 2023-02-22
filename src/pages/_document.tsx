6a34985ab4653cbff23fef56a5513d7e1a0c0a0c
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-22 21:07:31 +0300

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html lang="ru">
-			<Head />
+			<Head>
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+				<link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg"/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
