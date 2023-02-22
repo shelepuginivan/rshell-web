e681c0eab0df62760b4b137f8b6f1079745eab23
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-22 14:01:03 +0300

    added constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const downloadRoute = '/download'
+export const documentationRoute = '/docs'
+export const aboutRoute = '/about'
+export const sourceCodeRoute = 'https://github.com/shelepuginivan/rshell'
