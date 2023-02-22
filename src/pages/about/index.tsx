0272429edebd144e1eea0a5e4b255fe7f8d5677a
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-22 14:24:35 +0300

    added base routing

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import {FunctionComponent} from 'react'
+
+const Index: FunctionComponent = () => {
+	return (
+		<div>
+			<Head>
+				<title>О rshell</title>
+			</Head>
+		</div>
+	)
+}
+
+export default Index
