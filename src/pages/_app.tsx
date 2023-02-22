20bccbcf7db9f7c26d1d9c37e1e4d51fd3b8a330
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-22 14:03:13 +0300

    added style imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
+import '@/styles/global.sass'
+import '@/styles/reset.sass'
+import '@/styles/variables.sass'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
