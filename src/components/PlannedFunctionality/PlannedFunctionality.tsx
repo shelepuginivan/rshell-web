4058ce26a147272ecb8aa8baab5ea69535c2bab8
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-23 05:11:31 +0300

    created section about planned functionality

---
+++ src/components/PlannedFunctionality/PlannedFunctionality.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react'
+
+const PlannedFunctionality: FC = () => {
+	return (
+		<>
+			<h2>Запланированный функционал</h2>
+			<ul>
+				<li>Циклы</li>
+				<li>Логические выражения</li>
+				<li>Математика</li>
+			</ul>
+		</>
+	)
+}
+
+export default PlannedFunctionality
