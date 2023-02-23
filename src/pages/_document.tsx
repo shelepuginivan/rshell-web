d59c17ef553d0ac23700bf7422fe68423df7bfa2
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-23 23:45:17 +0300

    added meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
 			<Head>
 				<link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
 				<link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg"/>
+				<meta name="viewport" content="width=device-width, initial-scale=1"/>
 			</Head>
 			<body>
 				<Main />
