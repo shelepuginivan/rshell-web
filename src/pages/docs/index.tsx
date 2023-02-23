831c341f04bfeada5feee7413d6e5ac689f65f36
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-23 18:15:12 +0300

    included `Documentation` in docs page

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import {FC} from 'react'
 import Header from '@/components/Header/Header'
+import Documentation from '@/components/Documentation/Documentation'
 
 const Index: FC = () => {
 	return (
@@ -9,6 +10,7 @@ const Index: FC = () => {
 				<title>Документация -  rshell</title>
 			</Head>
 			<Header/>
+			<Documentation/>
 		</div>
 	)
 }
