366d8fe860043aa673bed1523cca0e473a5689ee
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-23 03:13:18 +0300

    imported fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/global.sass'
 import '@/styles/reset.sass'
 import '@/styles/variables.sass'
+import '../../public/assets/fonts/icomoon/style.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
