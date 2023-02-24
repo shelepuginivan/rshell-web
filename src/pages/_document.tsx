22a98cfa2a83ab8024c5a8a0be072257779b6e9a
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-24 14:07:22 +0300

    included more meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ export default function Document() {
 	return (
 		<Html lang="ru">
 			<Head>
+add				<meta charSet="utf-8"/>
 				<meta name="viewport" content="width=device-width, initial-scale=1"/>
+				<meta name="description" content="Российская командная оболочка для UNIX систем с открытым исходным кодом"/>
+				<meta name="keywords" content="rshell, shell, оболочка, UNIX, скриптовый язык, командная строка, язык программирования"/>
+				<meta name="author" content="Ivan Shelepugin <shelepuginivanm@gmail.com>"/>
+				<meta property="og:title" content="rshell - Командная оболочка UNIX"/>
+				<meta property="og:description" content="Российская командная оболочка для UNIX систем с открытым исходным кодом"/>
 				<link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
 				<link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg"/>
 				<link rel="manifest" href="/manifest.json"/>
