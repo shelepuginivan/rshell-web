6e8cf58dcb0afc4fc30f38a5888acec9d18b0b27
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-26 14:23:24 +0300

    implemented text typing animation on command with `IntersectionObserver`

--- src/components/SectionCommandInterpreter/SectionCommandInterpreter.tsx
+++ src/components/SectionCommandInterpreter/SectionCommandInterpreter.tsx
@@ -1,13 +1,64 @@
-import {FC} from 'react'
+import {FC, useEffect, useRef, useState} from 'react'
 import styles from './sectionCommandInterpreter.module.sass'
 
 const SectionCommandInterpreter: FC = () => {
+	const commandRef = useRef<HTMLSpanElement>(null)
+	const outputRef = useRef<HTMLSpanElement>(null)
+
+	const [commandText, setCommandText] = useState<string>('')
+	const [outputText, setOutputText] = useState<string>('')
+
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					observer.unobserve(commandRef.current as Element)
+
+					const text = 'echo Hello, World! | @format --red --bold --italic'
+					const limit = text.length
+					let iter = 0
+
+					const textAdditionInterval = setInterval(() => {
+						const updatedText = text.slice(0, iter)
+
+						setCommandText(updatedText)
+
+						if (updatedText.includes('echo ')) {
+							const output = (updatedText.split('|')[0]).replace('echo ', '')
+							setOutputText(output)
+						}
+
+						if (updatedText.includes('--red')) {
+							(outputRef.current as HTMLSpanElement).classList.add(styles.red)
+						}
+
+						if (updatedText.includes('--bold')) {
+							(outputRef.current as HTMLSpanElement).classList.add(styles.bold)
+						}
+
+						if (updatedText.includes('--italic')) {
+							(outputRef.current as HTMLSpanElement).classList.add(styles.italic)
+						}
+
+						if (iter >= limit) {
+							clearInterval(textAdditionInterval)
+						}
+
+						++iter
+					}, 90)
+				}
+			})
+		})
+
+		observer.observe(commandRef.current as Element)
+	}, [])
+
 	return (
 		<section className={styles.sectionCommandInterpreter}>
 			<code>
-				<span>$ echo Hello, World! | @format --red --bold --italic</span>
+				<span className={styles.command} ref={commandRef}>{commandText}</span>
 				<br/>
-				<span className={styles.formatted}>Hello, World!</span>
+				<span ref={outputRef}>{outputText}</span>
 			</code>
 
 			<h1>Командный интерпретатор</h1>
