494544885b53666073e361c8ea9f62ded6c3b0f1
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-26 14:36:45 +0300

    implemented text typing animation on command with `IntersectionObserver`

--- src/components/SectionScriptingLanguage/SectionScriptingLanguage.tsx
+++ src/components/SectionScriptingLanguage/SectionScriptingLanguage.tsx
@@ -1,13 +1,48 @@
-import {FC} from 'react'
+import {FC, useEffect, useRef, useState} from 'react'
 import styles from './sectionScriptingLanguage.module.sass'
 
 const SectionScriptingLanguage: FC = () => {
+	const commandRef = useRef<HTMLSpanElement>(null)
+
+	const [commandText, setCommandText] = useState<string>('')
+	const [outputText, setOutputText] = useState<string>('')
+
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					observer.unobserve(commandRef.current as Element)
+
+					const text = '@exec script.rsh'
+					let iter = 0
+
+					const textAdditionInterval = setInterval(() => {
+						const updatedText = text.slice(0, iter)
+						setCommandText(updatedText)
+
+						if (iter >= text.length) {
+							setOutputText('I\'m a scripting language!!')
+
+							clearInterval(textAdditionInterval)
+						}
+
+						iter++
+					}, 90)
+
+				}
+			})
+		})
+
+		observer.observe(commandRef.current as Element)
+	}, [])
+
 	return (
 		<section className={styles.scriptingLanguage}>
 			<code>
-				<span>$ @exec script.rsh</span>
+				<span ref={commandRef} className={styles.command}>{commandText}</span>
 				<br/>
-				<span>I&apos;m a scripting language!!</span>
+				<span>{outputText}</span>
 			</code>
 
 			<h1>...и скриптовый язык</h1>
